4e0919aa631afa412cf1eea22717031afb31310d
IPIvliev <cccp1945@mail.ru>
2023-03-22 17:55:47 +0300

    Change hosts in settings

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ if not SECRET_KEY:
 # Render Deployment Code
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = ["www.region52.su", "region52.su", "localhost", "127.0.0.1"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
